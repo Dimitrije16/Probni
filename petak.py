3831ac17466580e765fdda2645e33017e6d2596a
Dimitrije Maravić <dmaravic@smart.edu.rs>
2022-01-18 17:22:09 +0100

    init commit

---
+++ petak.py
@@ -0,0 +1,9 @@
+III2=["Georgije", "Petar", "Mina", "Uroš", "Vanja", "David", "Danilo", "Branislav", "Miloš", "Jovan", "Mirko", "Stefan"]
+print(len(III2))
+print(III2[4])
+print(III2[0:5])
+print(III2[:5])
+x=III2[4]
+y=III2[4:5]
+print(x,type(x),y , type(y))
+print("sunce " """"jeste" """"crno")
